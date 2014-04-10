ac330fdcd0c4b2c4f1ec804952db28a37362ac02
Ruijian Zhang@Tieto <pi@home2.(none)>
2014-04-10 16:06:43 +0800

    25

--- 25.py
+++ 25.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
-seq = [1,1]
+seq = (1,1)
+term = 2
 
-def expand_fib_seq(fib_seq):
+def fib_next_num(fib_seq):
+    num = sum(fib_seq)
+    return (fib_seq[1],num)
 
-
+while True:
+    seq = fib_next_num(seq)
+    term+=1
+    if len(str(seq[1])) >999:
+        print term
+        break
