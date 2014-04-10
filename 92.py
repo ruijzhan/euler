0377644551de193ec5bec75563e824705b3bea26
Ruijian Zhang <ruijian.zhang@qq.com>
2014-04-10 20:29:16 +0800

    92

---
+++ 92.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+def get_next(n):
+    return sum([int(x)**2 for x in str(n)])
+
+results = {}
+
+def get_end_number(n):
+    lis = [n]
+    n_current = n
+    while True:
+        n_next = get_next(n_current)
+        if n_next in lis:
+            for i in lis:
+                results[i]=n_next
+            return n_next
+        if n_next in results.keys():
+            for i in lis:
+                results[i]=n_next
+            return results[n_next]
+
+        n_current = n_next
+        lis.append(n_next)
+
+n=0
+for i in xrange(1,10000000):
+    if get_end_number(i) == 89:
+        n+=1
+
+print n
+
